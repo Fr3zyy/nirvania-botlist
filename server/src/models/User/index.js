2c3f01eecfbad84cfbbcc141a16b8c228830b0ee
Fr3zy <>
2024-08-04 21:57:02 +0300

    added user role

--- server/src/models/User/index.js
+++ server/src/models/User/index.js
@@ -1,5 +1,10 @@
 const mongoose = require('mongoose');
 
+const ROLES = {
+  USER: 'user',
+  ADMIN: 'admin',
+};
+
 const UserSchema = new mongoose.Schema({
   id: {
     type: String,
@@ -14,6 +19,11 @@ const UserSchema = new mongoose.Schema({
     type: String,
     default: null,
   },
+  roles: [{
+    type: String,
+    enum: Object.values(ROLES),
+    default: [ROLES.USER]
+  }],
   lastLogin: {
     type: Date,
     default: Date.now,
@@ -33,24 +43,30 @@ const UserSchema = new mongoose.Schema({
 }, {
   timestamps: true,
 });
+UserSchema.methods.hasRole = function(role) {
+  return this.roles.includes(role);
+};
 
-UserSchema.methods.toJSON = function() {
+UserSchema.methods.toJSON = function () {
   const user = this.toObject();
   delete user.__v;
   return user;
 };
 
-UserSchema.methods.isSubscriptionActive = function() {
-  return this.subscription.type !== 'free' && 
-         (!this.subscription.endDate || this.subscription.endDate > new Date());
+UserSchema.methods.isSubscriptionActive = function () {
+  return this.subscription.type !== 'free' &&
+    (!this.subscription.endDate || this.subscription.endDate > new Date());
 };
 
-UserSchema.methods.hasRole = function(role) {
-  return this.roles.includes(role);
+UserSchema.methods.hasRole = function (role) {
+  return this.roles.includes(role); 
 };
 
-UserSchema.statics.findByid = function(id) {
+UserSchema.statics.findByid = function (id) {
   return this.findOne({ id: id.toString() });
+};
+UserSchema.methods.isAdmin = function() {
+  return this.hasRole(ROLES.ADMIN);
 };
 
 module.exports = mongoose.model('User', UserSchema);
