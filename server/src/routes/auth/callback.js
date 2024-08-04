1f9224da81427bb011862dd862fafc82715d1177
Fr3zy <>
2024-08-04 23:09:20 +0300

    changed redirect url to frontEnd url.

--- server/src/routes/auth/callback.js
+++ server/src/routes/auth/callback.js
@@ -1,10 +1,11 @@
+const config = require('@/src/config/settings');
 const passport = require('passport');
 
 module.exports = {
   get: [
     passport.authenticate('discord', { failureRedirect: '/login' }),
     (req, res) => {
-      res.redirect('/');
+      res.redirect(config.website.frontEnd);
     }
   ]
 };
