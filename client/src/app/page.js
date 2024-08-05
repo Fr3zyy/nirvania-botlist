ebd80e49c2c9cdb4bf3c18a7493780f43d97de0a
Fr3zy <>
2024-08-05 13:14:39 +0300

    Changed component name to Home

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,6 +1,6 @@
 import Square from "@/components/Background/Square";
 
-export default function Index() {
+export default function Home() {
   return (
     <div>
       <Square column="6" row="6" zIndex={0} transparentEffectDirection="leftRightBottomTop" blockColor="#808080" />
