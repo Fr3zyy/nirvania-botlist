bf9c861ad3cc764c93e31647a6fb6a60262bb075
Fr3zy <>
2024-08-07 01:45:37 +0300

    deleted botTags and languages.

--- server/src/config/settings.js
+++ server/src/config/settings.js
@@ -10,7 +10,7 @@ const config = {
         scopes: ["identify"]
     },
     database: {
-        url: "mongodb://localhost:27017/server"
+        url: "mongodb://localhost:27017/botlist"
     },
     server: {
         id: "",
@@ -26,23 +26,6 @@ const config = {
     },
     website: {
         frontEnd: ["http://localhost:3000"],
-        botTags: [
-            "Fun", "NSFW", "Game", "24/7", "OSU!", "Guard", "Anime", "Music",
-            "Memes", "Invite", "Reddit", "Twitch", "Crypto", "Economy",
-            "Logging", "Youtube", "Utility", "General", "Leveling", "Roleplay",
-            "Fortnite", "Welcomer", "Chat bot", "Minecraft", "Community",
-            "Minigames", "Moderation", "Protection", "Web Dashboard",
-            "Reaction Roles", "Auto Moderation"
-        ],
-        languages: [
-            { flag: 'tr', code: 'tr', name: 'Türkce' },
-            { flag: 'gb', code: 'en', name: 'English' },
-            { flag: 'de', code: 'de', name: 'Deutsch' },
-            { flag: 'ru', code: 'ru', name: 'Russian' },
-            { flag: 'fr', code: 'fr', name: 'French' },
-            { flag: 'es', code: 'es', name: 'Spanish' },
-            { flag: 'gr', code: 'el', name: 'Greek' }
-        ]
     }
 };
 
