969c84a8975b3e881d7504915c3215a968c39489
Fr3zy <>
2024-08-07 01:42:21 +0300

    updated header

--- client/src/components/Header/DesktopNavbar.js
+++ client/src/components/Header/DesktopNavbar.js
@@ -12,7 +12,7 @@ const DesktopNavbar = () => {
           whileHover={{ scale: 1.05 }}
           whileTap={{ scale: 0.98 }}
         >
-          <Link href={item.target} className="flex items-center space-x-1 text-gray-400 hover:text-indigo-500 transition-colors duration-200 text-lg">
+          <Link href={item.target} className="flex items-center space-x-1 text-gray-400 hover:text-indigo-500 transition-colors duration-200 text-sm">
             <item.icon className="w-4 h-4" />
             <span>{item.label}</span>
           </Link>
